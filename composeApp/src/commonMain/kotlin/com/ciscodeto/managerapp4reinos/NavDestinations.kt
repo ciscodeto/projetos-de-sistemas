0e8b9990aaebf09574f95a22efa8cb6aa1cb80a5
ciscodeto <netofcisco@gmail.com>
2025-05-13 20:13:11 -0300

    feat: small changes to ui on ComposeApp

---
+++ composeApp/src/commonMain/kotlin/com/ciscodeto/managerapp4reinos/NavDestinations.kt
@@ -0,0 +1,51 @@
+package com.ciscodeto.managerapp4reinos
+
+import kotlinx.serialization.Serializable
+
+sealed interface NavDestinations {
+    @Serializable
+    data object HomeScreen : NavDestinations
+
+    @Serializable
+    data class CreateCharacterScreen(val characterId: String) : NavDestinations
+
+    @Serializable
+    data class EditCharacterScreen(val characterId: String) : NavDestinations
+
+    @Serializable
+    data class CharacterDetailScreen(val characterId: String) : NavDestinations
+
+    @Serializable
+    data object CharactersListScreen : NavDestinations
+
+    @Serializable
+    data class CreateItemScreen(val characterId: String? = null) : NavDestinations
+
+    @Serializable
+    data class EditItemScreen(val itemId: String) : NavDestinations
+
+    @Serializable
+    data object ItemsListScreen : NavDestinations
+
+    @Serializable
+    data class ActionSelectionScreen(val characterId: String) : NavDestinations
+
+    @Serializable
+    data class TargetSelectionScreen(val actionId: String, val sourceCharacterId: String) : NavDestinations
+
+    @Serializable
+    data class ActionExecutionScreen(val actionId: String, val sourceCharacterId: String, val targetCharacterId: String?) : NavDestinations
+
+    @Serializable
+    data class CounterActionScreen(
+        val originalActionId: String,
+        val responderId: String,
+        val initiatorId: String
+    ) : NavDestinations
+
+    @Serializable
+    data object ActionLogScreen : NavDestinations
+
+    @Serializable
+    data object SettingsScreen : NavDestinations
+}
--- composeApp/src/commonMain/kotlin/com/ciscodeto/managerapp4reinos/NavGraph.kt
+++ composeApp/src/commonMain/kotlin/com/ciscodeto/managerapp4reinos/NavGraph.kt
@@ -1,2 +1,15 @@
 package com.ciscodeto.managerapp4reinos
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraph
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun NavGraph(
+    navController: NavHostController,
+) {
+    NavHost(navController = navController, startDestination = "home") {
+
+    }
+}
